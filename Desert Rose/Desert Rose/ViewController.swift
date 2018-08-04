00b450d6483026032c89af791ce1432c942077c8
Paul Northup <paul@paulnorthup.com>
2018-08-04 08:56:46 -0400

    add crappy ios proj

---
+++ Desert Rose/Desert Rose/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Desert Rose
+//
+//  Created by Paul Northup on 8/3/18.
+//  Copyright © 2018 Paul Northup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
